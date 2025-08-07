676a64de87fb36c99a01b67905b0c4a0e286261d
Hamza-theGOAT <hamza.b0302@gmail.com>
2025-08-07 19:59:19 +0500

    cleaner implementation with numeric formatting

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from openpyxl import load_workbook
 # Validate range params and pick max if not given
 
 
-def getRange(ws, r0, r1, c0, c1):
+def getRange(ws, r0=None, r1=None, c0=None, c1=None):
     r0 = r0 if r0 is not None else ws.min_row
     r1 = r1 if r1 is not None else ws.max_row
     c0 = c0 if c0 is not None else ws.min_col
     c1 = c1 if c1 is not None else ws.max_col
 
-    return r0, r1, c0, c1
+    return {
+        'r0': r0, 'r1': r1, 'c0': c0, 'c1': c1
+    }
 
 
 def AltA(ws):
@@ -35,6 +37,18 @@ def AltHOI(ws):
     # Add a little extra width
     for col, width in colWidths.items():
         ws.column_dimensions[col].width = width + 3
+
+
+def AltHK(ws, rg=None):
+    # if list of range (rg) is missing, use max ranges
+    if rg is None:
+        rg = getRange(ws)
+
+    for col in ws.iter_cols(
+            min_row=rg['r0'], max_row=rg['r1'],
+            min_col=rg['c0'], max_col=rg['c1']):
+        for cell in col:
+            cell.number_format = '#,##0;(#,##0);0'
 
 
 def tableFormatWB(wb):
@@ -61,38 +75,48 @@ def tableFormatWS(ws):
     ws = AltHOI(ws)
 
 
-def formatWS(wb, shz=[]):
+def formatWS(wb, shz=None):
+    # if list of sheets not provided, pick all sheets in wb
     if shz is None:
         shz = wb.sheetnames
 
+    # loop through all sheets in the given list
     for sh in shz:
         ws = wb[sh]
         tableFormatWS(ws)
 
 
-def formatWB(wbNin, wbNout):
+def formatWB(wbNin, wbNout=None, ty=None, shz=None):
     wb = load_workbook(wbNin)
 
-    # Variable to control formatting function choice
-    ty = 'wb'
-
+    # Format all sheets in wb with sheetname loop
     if ty == 'wb':
-        # Format all sheets in wb with sheetname loop
         tableFormatWB(wb)
 
+    # Format all sheets in wb with additional control
     if ty == 'ws':
-        # Format all sheets in wb with additional control
-        formatWS(wb)
-
-    if ty == 'wsL':
-        # Format all sheets in list of sheets
-        shz = ['Sheet1', 'Sheet3']
         formatWS(wb, shz)
+
+    # To replace the formatted wb with original
+    if wbNout is None:
+        wbNout = wbNin
 
     wb.save(wbNout)
 
 
-if __name__ == '__main__':
+def main():
     wbNin = 'fileInput.xlsx'
     wbNout = 'fileOutput.xlsx'
-    formatWB(wbNin, wbNout)
+
+    # Variable to control formatting function choice
+    ty = 'wb'
+
+    # Creating the list of desired sheets
+    shz = ['Sheet1', 'Sheet3']
+
+    # Formate sheets accordingly
+    formatWB(wbNin, wbNout, ty, shz)
+
+
+if __name__ == '__main__':
+    main()
